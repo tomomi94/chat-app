b75046216b9dd76edd57fc8ccf0dc2e61c6aea02
tomomi94 <kurochan1004@gmail.com>
2021-02-11 15:50:20 +0900

    メッセージ送信機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :room_users
   has_many :rooms, through: :room_users
-  has_mane :messages
+  has_many :messages
 
   validates :name, presence: true
 end
